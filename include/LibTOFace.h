227e14939eef5101a75eab7309a5ec30bb3de76c
TinyOh <tinyoh@slightech.com>
2018-12-26 16:32:51 +0800

    style(interface) simplify the API

--- include/LibTOFace.h
+++ include/LibTOFace.h
@@ -61,52 +61,6 @@ extern "C" {
     //结束
      */
     TO_LIB_STATUS TO_Face_Tracker_Stop();
-
-
-
-    //=================================Plans are no longer available==================================
-
-
-
-    /*
-    //开始 (Plans are no longer available)
-     */
-    TO_LIB_STATUS TO_Face_Tracker_Start(
-        // these 3 Cascade  path is the main detect tool of face detect. 
-        const char *faceCascadeFilename,
-        // 线程的休眠间隔最小单位，就是说隔多久对整体的状态做一次反应，不可设置太大 单位：us
-        const unsigned long threadsWaitTimeInterval,
-        // 脸部检测休眠检测判定需要的检测失败次数 
-        const unsigned short faceDetectCountEnableThreshold,
-        // 自适应的主检测更新策略阀值 
-        const int updateStrategy,
-        //更新限制时间
-        const int updateStrategyTime,
-        //宽
-        const size_t width,
-        //高
-        const size_t height,
-        //最高脸部检测数量上限
-        const size_t count
-        );
-
-    /*
-    //当前是否存在脸(Plans are no longer available)
-     */
-    TO_LIB_STATUS TO_Face_is_Face_Exist_Now();
-
-
-    /*
-    //实时处理简化接口(包含TO_Face_Tracker_Sampling 和 TO_Face_Tracker_Check 功能 ，为单帧非异步调用提供接口)(Plans are no longer available)
-    */
-    TO_LIB_STATUS TO_Face_Tracker_Update(
-        //脸部位置框结果输出
-        std::vector<TO_RECT>& face,
-        //阻塞超时阀值
-        const unsigned int blockWaitTime, // ms
-        //测试结果显示输出，NULL时不做结果显示工作，在主检测口会增加耗时～5ms
-        unsigned char* pBGRA);
-
 #ifdef __cplusplus
 }
 #endif
--- include/LibTOFaceDebug.h
+++ include/LibTOFaceDebug.h
@@ -40,43 +40,6 @@ extern "C" {
         std::vector<TO_LANDMARK_68>& landMark,
         //测试结果显示输出，NULL时不做结果显示工作，在主检测口会增加耗时～5ms
         unsigned char* pBGRA);
-
-
-    //=================================Plans are no longer available==================================
-
-    /*
-    //实时处理接口 (Plans are no longer available)
-     */
-    TO_LIB_STATUS TO_Face_Tracker_Check(
-        //脸部位置框结果输出
-        std::vector<TO_RECT>& face,
-        //阻塞超时阀值
-        const unsigned int blockWaitTime, // ms
-        //测试结果显示输出，NULL时不做结果显示工作，在主检测口会增加耗时～5ms
-        unsigned char* pBGRA);
-
-    /*
-    //肤色滤波 (Plans are no longer available)
-     */
-    TO_LIB_STATUS TO_Face_Skin_Filter(
-        //输出结果
-        cv::Mat& out,
-        //输入
-        cv::Mat& in,
-        //滤波器种类
-        TO_SKIN_FILTER_KIND filtet
-        );
-
-    /*
-    //停止线程，保留类内变量资源 (Plans are no longer available)
-     */
-    TO_LIB_STATUS TO_Face_Tracker_Pause();
-
-    /*
-    //使用类内变量资源启动线程 (Plans are no longer available)
-     */
-    TO_LIB_STATUS TO_Face_Tracker_Resume();
-
 #ifdef __cplusplus
 }
 #endif
